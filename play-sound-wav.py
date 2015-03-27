0e9bdae5f2152a5f0b6cff95100ced6ca9f68e28
Vlad Dracula <vlad@tran.sylvan.ia>
2015-03-27 11:49:16 +0000

    added basic python file that reads a profile datafile, scales it, and writes out a file formated for chuck

---
+++ play-sound-wav.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import numpy
+
+# open a profile file (will lead coupling to getting the data from Zenodo)
+INPUT = open('profiles/a135t-profile.dat')
+
+profile = []
+x = numpy.arange(128)
+
+while True:
+    
+    # read in the next line
+    line = INPUT.readline()
+    
+    # if there is no line, leave the loop
+    if not line:
+        break
+        
+    profile.append(float(line))
+
+# save the data to a numpy array    
+y = numpy.array(profile)
+
+# rescale the data so zero is 60, which is middle C
+y+=200
+y/=400.
+z = numpy.int16(y * 120)
+
+# create the line needed for chuck
+line = "%5i" % len(z) 
+for i in z:
+    line += "%3i " % i
+print line
+    
